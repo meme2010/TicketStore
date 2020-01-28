25ff8adc8c93a73bd9e65041c9b7379e44d87454
emerson <emerson@dekbed-discounter.nl>
2020-01-28 15:05:45 +0100

    Instalation of laravel ui and phpspec

---
+++ spec/UserSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\App;
+
+use App\User;
+use PhpSpec\ObjectBehavior;
+
+class UserSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(User::class);
+    }
+}
